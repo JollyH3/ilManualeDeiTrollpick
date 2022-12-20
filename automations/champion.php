11c8766213a16ac5176a5e07729c94d2cf2949af
Giosuè Iaccarino <giosue@iacca.ml>
2022-12-20 21:56:32 +0100

    🧐 Data insertion scripts

---
+++ automations/champion.php
@@ -0,0 +1,35 @@
+<?php
+
+$data = file_get_contents("http://ddragon.leagueoflegends.com/cdn/12.23.1/data/it_IT/champion.json");
+$data = json_decode($data, true);
+
+$pdo = new PDO("mysql:host=localhost;dbname=trollpick", "jolly", "Yb68#53SxQtA");
+$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$pdo->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);
+$pdo->query('set profiling=1');
+
+$data = $data['data'];
+
+
+foreach ($data as $champion) {
+	
+	$champion_data = json_decode(file_get_contents("http://ddragon.leagueoflegends.com/cdn/12.23.1/data/it_IT/champion/" . $champion['id'] . ".json"), true);
+	
+	$version = $champion_data['version'];
+	$champion_data = $champion_data['data'][$champion['id']];
+
+	$stmt = $pdo->prepare("INSERT IGNORE INTO champion VALUES(:champion_id, :name, :image, :splash, :info, :stats, :spells, :passive, :version);");
+	$stmt->execute([
+		'champion_id' => intval($champion['key']),
+		'name' => $champion['name'],
+		'image' => "http://ddragon.leagueoflegends.com/cdn/12.23.1/img/champion/" . $champion['image']['full'],
+		'splash' => "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/" . $champion['id'] . "_0.jpg",
+		'info' => json_encode($champion['info']),
+		'stats' => json_encode($champion['stats']),
+		'spells' => json_encode($champion_data['spells']),
+		'passive' => json_encode($champion_data['passive']),
+		'version' => $version,
+	]);
+
+	echo $champion['name'] . PHP_EOL;
+}
---
+++ automations/item.php
@@ -0,0 +1,39 @@
+<?php
+
+$data = file_get_contents("http://ddragon.leagueoflegends.com/cdn/12.23.1/data/it_IT/item.json");
+$data = json_decode($data, true);
+
+$pdo = new PDO("mysql:host=localhost;dbname=trollpick", "root", "");
+$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$pdo->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);
+$pdo->query('set profiling=1');
+
+$version = $data['version'];
+$data = $data['data'];
+
+foreach ($data as $item_id => $item) {
+
+	$stmt = $pdo->prepare("INSERT IGNORE INTO item VALUES(:item_id, :name, :image, :gold, :description, :stats, :version);");
+	$stmt->execute([
+		'item_id' => intval($item_id),
+		'name' => strip_tags($item['name']),
+		'image' => "http://ddragon.leagueoflegends.com/cdn/12.23.1/img/item/" . $item['image']['full'],
+		'gold' => json_encode($item['gold']),
+		'description' => $item['description'],
+		'stats' => json_encode($item['stats']),
+		'version' => $version,
+	]);
+
+
+	if (!empty($item['into'])) {
+		for ($i = 0; $i < count($item['into']); $i++) {
+			$stmt = $pdo->prepare("INSERT IGNORE INTO item_build VALUES(:parent_item_id, :child_item_id);");
+			$stmt->execute([
+				'parent_item_id' => intval($item['into'][$i]),
+				'child_item_id' => intval($item_id),
+			]);
+		}
+	}
+
+	echo $item['name'] . PHP_EOL;
+}
